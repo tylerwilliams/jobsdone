99df968278b87bd89ab58e76a0bf4fcb24fd8e7f
Tyler Williams <tylerw@tylers-macbook.lan>
2019-09-22 18:26:24 -0700

    Correctly set install_requires.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "jobsdone",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Tyler Williams",
     author_email = "williams.tyler@gmail.com",
     description = ("A little helper to send you a push when your job"
@@ -18,4 +18,8 @@ setup(
     scripts=['jobsdone/jd'],
     long_description=read('README.md'),
     long_description_content_type="text/markdown",
+    install_requires=[
+      'appdirs',
+      'requests',
+    ],
 )
